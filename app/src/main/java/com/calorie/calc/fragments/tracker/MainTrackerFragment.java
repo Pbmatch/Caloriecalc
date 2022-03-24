fd8be92c2797444a4808c656c71e51432e659448
Pb <apm-nt@yandex.ru>
2022-03-24 18:08:29 +0600

    Дизаин главной счетчика

---
+++ app/src/main/java/com/calorie/calc/fragments/tracker/MainTrackerFragment.java
@@ -0,0 +1,35 @@
+package com.calorie.calc.fragments.tracker;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.calorie.calc.R;
+
+
+public class MainTrackerFragment extends Fragment {
+
+
+    public MainTrackerFragment() {
+
+    }
+
+
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.fragment_main_tracker, container, false);
+    }
+}
